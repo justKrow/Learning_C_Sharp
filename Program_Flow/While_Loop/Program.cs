3eb91a45a3eddf8f4b97403573ee0458c8072f0c
justKrow <krowxen2214@gmail.com>
2025-03-01 13:16:36 +0000

    while loop

---
+++ Program_Flow/While_Loop/Program.cs
@@ -0,0 +1,16 @@
+﻿string inputString = "";
+
+// Console.WriteLine("-------------Basic While Loop------------");
+
+// while (inputString != "exit")
+// {
+//     inputString = Console.ReadLine();
+//     Console.WriteLine("You entered: " + inputString);
+// }
+
+Console.WriteLine("-------------Do While Loop------------");
+do
+{
+    inputString = Console.ReadLine();
+    Console.WriteLine("You entered: " + inputString);
+} while (inputString != "exit");
