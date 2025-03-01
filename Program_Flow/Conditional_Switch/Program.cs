35fcc4427703a13315d73b8037a77b730fbddcda
justKrow <krowxen2214@gmail.com>
2025-03-01 12:06:48 +0000

    switch

---
+++ Program_Flow/Conditional_Switch/Program.cs
@@ -0,0 +1,19 @@
+﻿int val = 50;
+
+switch (val)
+{
+    case 50:
+        Console.WriteLine("Value is 50");
+        break;
+    case 40:
+        Console.WriteLine("Value is 40");
+        break;
+    case 55:
+    case 56:
+    case 57:
+        Console.WriteLine("Value is 55, 56, or 57");
+        break;
+    default:
+        Console.WriteLine("Value is not 40 or 50");
+        break;
+}
