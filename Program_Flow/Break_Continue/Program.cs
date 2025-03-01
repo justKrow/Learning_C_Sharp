f9295649fc5bbcba819c8c113544b3b6bbfa86ed
justKrow <krowxen2214@gmail.com>
2025-03-01 16:59:54 +0000

    break and continue

---
+++ Program_Flow/Break_Continue/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] values = { 15, 24, 5, 89, 43, 12 };
+
+Console.WriteLine("---------------Using break and continue----------");
+
+foreach (int val in values)
+{
+    if (val <= 50)
+    {
+        continue;
+    }
+
+    Console.WriteLine("Val is currently {0}", val);
+
+    if (val > 30)
+    {
+        break;
+    }
+}
