d76636d16a639193e8b1bd8765c9805818981028
justKrow <krowxen2214@gmail.com>
2025-03-01 17:24:29 +0000

    exception handling

---
+++ Program_Flow/Exception/Program.cs
@@ -0,0 +1,25 @@
+﻿int x = 1001, y = 0;
+int result;
+
+try
+{
+    if (x > 1000)
+    {
+        throw new ArgumentException("x", "x must be 1000 or less");
+    }
+    result = x / y;
+    Console.WriteLine("Result of division is {0}", result);
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine("An error occurred during division");
+    Console.WriteLine(e.Message);
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine("Error Message: {0}", e.Message);
+}
+finally
+{
+    Console.WriteLine("This code block will always execute");
+}
