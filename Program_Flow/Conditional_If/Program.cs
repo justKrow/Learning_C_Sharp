a06bec33d59ba37bd6d23f63238d452365e21bdc
justKrow <krowxen2214@gmail.com>
2025-03-01 12:00:10 +0000

    conditional and ternary

---
+++ Program_Flow/Conditional_If/Program.cs
@@ -0,0 +1,18 @@
+﻿int val = 40;
+
+// if else
+if (val == 40)
+{
+    Console.WriteLine("Value is 40");
+}
+else if (val > 40)
+{
+    Console.WriteLine("Value is greater than 40");
+}
+else
+{
+    Console.WriteLine("Value is not 40");
+}
+
+// ternary operator
+Console.WriteLine((val < 40) ? "value is small" : "value is large");
