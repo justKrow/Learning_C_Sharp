5ecc8a445719408ab7f06c2bf5c9008f99f2ccad
justKrow <krowxen2214@gmail.com>
2025-03-02 03:33:07 +0000

    basics of function

---
+++ Functions/Basics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Functions are used to group re-usable code together in a single
+// unit that can be customized with parameters.
+
+// TODO: Functions have a return type, name, and optional parameters
+float MilesToKM(float miles)
+{
+    return miles * 1.60934f;
+}
+
+// TODO: A function with no return value has a 'void' type
+void PrintWithPrefix(string str)
+{
+    Console.WriteLine($"::>{str}");
+}
+
+// TODO: Call first function
+Console.WriteLine($"Result is : {MilesToKM(12f)}");
+Console.WriteLine($"Result is : {MilesToKM(2.21f)}");
+
+// TODO: Call second function
+PrintWithPrefix("This is a test");
+PrintWithPrefix("Another test");
